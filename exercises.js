3bb54608d50feb1407e0ebba0cf25c8f1f5cd308
Nicholas Gambino <nicholasgambino@Nicholass-MacBook-Pro.local>
2018-06-11 13:21:20 -1000

    34

--- exercises.js
+++ exercises.js
@@ -533,7 +533,17 @@ var mapArrayValues = function(arr){
  * @param {Array}
  * @return {Object}
  */
-var mapStringCounts;
+var mapStringCounts = function(arr){
+  newObj = {};
+  for (i=0; i<arr.length; i++){
+    if (arr[i].length >= 5){
+      newObj[arr[i]] = true;
+    }else{
+      newObj[arr[i]] = false;
+    }
+  }
+  return newObj;  
+}
 
 /* #arrayToObjectNums
  *
@@ -618,7 +628,7 @@ module.exports = {
   arraysToObject: arraysToObject,
   objectsToTuples: objectsToTuples,
   mapArrayValues: mapArrayValues,
-  mapStringCounts: null,
+  mapStringCounts: mapStringCounts,
   arrayToObjectNums: null,
   stringToKeys: null,
   charCountMap: null,
